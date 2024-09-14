874f39d17e52b533c965d64ce122eb14c0c24f8b
nerdcat <nerdcat.plays@gmail.com>
2024-09-14 21:23:19 +0300

    Did something

--- src/window.cpp
+++ src/window.cpp
@@ -64,3 +64,10 @@ bool window::releaseWindow() {
     framebuffer_ = nullptr;
     return true;
 }
+void window::runMessageLoop() {
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
